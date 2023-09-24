50e45374da949b72bce4c0a23edf042c6fcb592b
LibeerDev <libeergithub@gmail.com>
2023-09-24 02:04:06 +0200

    added cool homepage

---
+++ script.js
@@ -0,0 +1,10 @@
+allLinks = document.querySelectorAll('a');
+
+let lightness = 0;
+let saturation = 0;
+
+allLinks.forEach((link) => {
+    link.style.backgroundColor = `hsl(297, ${saturation}%, ${lightness}%)`
+    lightness += 1;
+    saturation += 1;
+})
