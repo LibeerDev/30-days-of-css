34fa6e453ba67854e5085988db9805317158fc25
LibeerDev <libeergithub@gmail.com>
2023-09-24 03:14:07 +0200

    undo

--- script.js
+++ script.js
@@ -8,24 +8,3 @@ allLinks.forEach((link) => {
     lightness += 1;
     saturation += 1;
 });
-
-allLinks.forEach((link) => {
-    link.addEventListener('click', (e) => {
-        const folderPath = link.getAttribute('href');
-
-        // Make an HTTP request to check if the folder exists
-        fetch(folderPath, { method: 'HEAD' })
-          .then(response => {
-            if (response.status !== 200) {
-              // Folder doesn't exist, prevent the default behavior and show an alert
-              alert("This day hasn't been added yet.");
-              e.preventDefault(); // Prevent navigation
-            }
-          })
-          .catch(error => {
-            // Handle any errors that occur during the fetch
-            console.error("Error:", error);
-          });
-          return false;
-    });
-});
